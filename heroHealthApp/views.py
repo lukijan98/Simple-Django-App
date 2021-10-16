44386518f1fb88fe17e3655eaa5e2ad3cade3b8c
Luka <jelicl@ymail.com>
2021-10-16 22:26:28 +0200

    finished update from device 3

--- heroHealthApp/views.py
+++ heroHealthApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import status
 from rest_framework import permissions
 
 from rest_framework.decorators import api_view, permission_classes
@@ -19,16 +20,14 @@ def frontendConfig(request, device_id):
     except Config.DoesNotExist:
         config = None
     device = Device.objects.get(id=device_id)
-    serializer = None
     if config is None:
         serializer = ConfigFrontendSerializer(data=request.data, context={'device': device})
     else:
         serializer = ConfigFrontendSerializer(instance=config, data=request.data, context={'device': device})
     if serializer.is_valid():
-        config = serializer.save()
-        serializer = ConfigFrontendSerializer(config)
-        return Response(serializer.data)
-    return Response("Failed")
+        if serializer.save() is not None:
+            return Response(status.HTTP_200_OK)
+    return Response(status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
@@ -44,23 +43,21 @@ def deviceConfig(request, device_id):
     else:
         serializer = ConfigDeviceSerializer(instance=config, data=request.data, context={'device': device})
     if serializer.is_valid():
-        config = serializer.save()
-        serializer = ConfigFrontendSerializer(config)
-        return Response(serializer.data)
-
-    return Response("Failed")
-
+        if serializer.save() is not None:
+            return Response(status.HTTP_200_OK)
+    return Response(status.HTTP_404_NOT_FOUND)
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def configList(request):
-	configs = Config.objects.all().order_by('-id')
-	serializer = ConfigFrontendSerializer(configs, many=True)
-	return Response(serializer.data)
+    configs = Config.objects.all().order_by('-id')
+    serializer = ConfigFrontendSerializer(configs, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 @permission_classes((permissions.AllowAny,))
 def configDetail(request, id):
-	configs = Config.objects.get(id=id)
-	serializer = ConfigFrontendSerializer(configs, many=False)
-	return Response(serializer.data)
+    configs = Config.objects.get(id=id)
+    serializer = ConfigFrontendSerializer(configs, many=False)
+    return Response(serializer.data)
